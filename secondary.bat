1fb7da46815688f43344aa749e569fe1e06a8d1f
ynb <106879946+yanibornea@users.noreply.github.com>
2024-06-28 18:25:00 +0300

    Update secondary.bat

--- secondary.bat
+++ secondary.bat
@@ -1,5 +1,5 @@
 @echo off
-@mode con cols=125 lines=35
+@mode con cols=130 lines=35
 title YNB account gen
 color D
 :start
@@ -23,18 +23,28 @@ color D
 :::                                                               
 for /f "delims=: tokens=*" %%A in ('findstr /b ::: "%~f0"') do @echo(%%A
 echo.
-echo Made by ynb21 on discord
-echo 1) Riot accounts, if accounts dont work regenerate (Valorant, LOL, etc)
-echo 2) more coming soon also dont change passwords please
+echo Made by ynb21 on discord, credits to shinycorp on discord (https://sigmapowers.lol/) for the accounts
+echo 1) Riot accounts, (Valorant, LOL, etc) (330)
+echo 2) Gunslol accounts, (70)
+echo 3) Steam accounts, (2)
+echo 4) Twitch accounts, (1)
+echo 5) more coming soon, if passwords dont work regenerate also dont change passwords please
+echo 6) also take in mind this is for valorant so most of these dont work.
 set /p type=Choose: 
 
 if "%type%"=="1" goto riot
+if "%type%"=="2" goto gunslol
+if "%type%"=="3" goto steam
+if "%type%"=="4" goto twitch
 
 goto end
 
 :riot
 :: Define list of lines
-set lines[1]=Please.Download.From.Packages
+Download from releases 
+
+
+
 
 :: Get the total number of lines
 set /a max=5
@@ -46,6 +56,54 @@ set /a index=%random% %% max
 call echo %%lines[%index%]%%
 
 goto end
+
+:gunslol
+:: Define list of lines
+
+
+:: Get the total number of lines
+set /a max=2
+
+:: Generate a random index
+set /a index=%random% %% max + 1
+
+:: Display the random line
+call echo %%lines[%index%]%%
+
+pause
+goto :eof
+
+:steam
+:: Define list of lines
+
+
+:: Get the total number of lines
+set /a max=2
+
+:: Generate a random index
+set /a index=%random% %% max + 1
+
+:: Display the random line
+call echo %%lines[%index%]%%
+
+pause
+goto :eof
+
+:twitch
+:: Define list of lines
+
+
+:: Get the total number of lines
+set /a max=2
+
+:: Generate a random index
+set /a index=%random% %% max + 1
+
+:: Display the random line
+call echo %%lines[%index%]%%
+
+pause
+goto :eof
 
 :end
 pause
